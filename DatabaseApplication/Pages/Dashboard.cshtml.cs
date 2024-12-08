e49c7fcb9a0e7a8d99b2b88157d5977ecfb412ba
Ishau <ishau@Ishaus-MBP.attlocal.net>
2024-12-08 12:47:16 -0600

    pages

---
+++ DatabaseApplication/Pages/Dashboard.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using DatabaseApplication.Pages.PageRouting;
+using DatabaseApplication.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DatabaseApplication.Pages
+{
+    public class DashboardModel : PageModel
+    {
+public string? RequestId { get; set; }
+
+  public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+  private readonly ILogger<DashboardModel> _logger;
+
+  public DashboardModel(ILogger<DashboardModel> logger)
+  {
+      _logger = logger;
+  }
+
+  public void OnGet()
+  {
+     
+  }
+    }
+}
